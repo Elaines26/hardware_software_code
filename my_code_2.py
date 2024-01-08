25d675d75ffb054bbd269c14e55b4fa8a1dd97e8
Elaine Sanchez <ethesniper@Elaines-MacBook-Pro.local>
2024-01-08 14:31:58 -0800

    first commit

---
+++ my_code_2.py
@@ -0,0 +1,9 @@
+def main() :
+    name = input ("what is your name?")
+    print("we want to know if you like programming! ")
+    answer = input("do you like programming "+ name + "?")
+    print('great! you said "'+ answer + '"?')
+    print("lets's start learning some python today")
+
+if __name__ == "__main__" :
+    main()
